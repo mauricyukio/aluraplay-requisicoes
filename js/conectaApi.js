d1489bfb5c3cc760108162bc1e9912ec9b220245
mauricyukio <myynagashima@gmail.com>
2022-12-22 10:59:40 -0300

    fim aula 3

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -5,4 +5,22 @@ async function listaVideos() {
   return conexaoConvertida
 }
 
-export const conectaApi = { listaVideos }
+async function criaVideo(titulo, descricao, url, imagem) {
+  const conexao = await fetch('http://localhost:3000/videos', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      titulo: titulo,
+      descricao: `${descricao} mil visualizações`,
+      url: url,
+      imagem: imagem,
+    }),
+  })
+
+  const conexaoConvertida = await conexao.json()
+  return conexaoConvertida
+}
+
+export const conectaApi = { listaVideos, criaVideo }
